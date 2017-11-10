8f930e18e0926c2d65a7989bf722ac1e0198d07e
xhl <1207944088@qq.com>
2017-11-10 17:06:21 +0800

    modify login.js

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,21 +1,38 @@
 import {login} from '../service/api';
 
 export const LOGIN_SUBMIT_START = 'LOGIN_SUBMIT_START';
-export const LOGIN_SUBMIT_END = 'LOGIN_SUBMIT_END';
-export const LOGIN_SUBMIT = 'LOGIN_SUBMIT';
+export const LOGIN_SUBMIT_SUCCESS = 'LOGIN_SUBMIT_SUCCESS';
+export const LOGIN_SUBMIT_ERROR = 'LOGIN_SUBMIT_ERROR'
 
-export const LOGIN_SUBMIT_PROMISE = 'LOGIN_SUBMIT_PROMISE';
 
 export const loginStart = () =>{
   type:LOGIN_SUBMIT_START
 }
 
-export const loginPromise = (form) => {
+export const loginSuccess = (resJson) => {
+  type:LOGIN_SUBMIT_SUCCESS,
+  payload:resJson
+}
+
+export const loginError = () => {
+  type:LOGIN_SUBMIT_ERROR,
+  payload:err
+}
+
+export const loginChunk = (form) => {
   return (dispatch,getState) => {
-    dispatch(loginStart);
-    dispatch({
-       type:LOGIN_SUBMIT_PROMISE,
-       payload:login(form)
+    dispatch(loginStart());
+    login(form).then((resJson) => {
+      if (resJson.OK) {
+        dispatch(loginSuccess(resJson.user));
+      } else {
+        message.error(resJson.message);
+        return dispatch(loginError(resJson.message));
+      }
+    }).catch((err) => {
+      message.error(err.toString());
+      return dispatch(loginError(err.toString()))
     })
   }
 }
+
